c54547cec2c81b66085a3b018a55322481fa0b89
GitHayato <gabayt0_0@icloud.com>
2023-07-01 21:38:00 +0900

    configure prettier

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,16 +2,6 @@
  * @type {import('prettier').Options}
  */
 module.exports = {
-  printWidth: 80,
-  tabWidth: 2,
-  useTabs: false,
-  semi: false,
-  singleQuote: false,
-  trailingComma: "none",
-  bracketSpacing: true,
-  bracketSameLine: true,
+  trailingComma: "all",
   plugins: [require.resolve("@plasmohq/prettier-plugin-sort-imports")],
-  importOrder: ["^@plasmohq/(.*)$", "^~(.*)$", "^[./]"],
-  importOrderSeparation: true,
-  importOrderSortSpecifiers: true
-}
+};
