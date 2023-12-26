b6bb359eb6e10351f8753356b921241a6e404bc1
GitHayato <gabayt0_0@icloud.com>
2023-12-26 22:57:04 +0900

    create Label

---
+++ src/components/Label/Label.tsx
@@ -0,0 +1,14 @@
+import * as styles from "./Label.module.pcss";
+
+import type { ComponentPropsWithoutRef } from "react";
+
+type Props = ComponentPropsWithoutRef<"label">;
+
+export const Label = ({ children, ...props }: Props) => {
+  return (
+    // eslint-disable-next-line jsx-a11y/label-has-associated-control
+    <label className={styles.root} {...props}>
+      {children}
+    </label>
+  );
+};
